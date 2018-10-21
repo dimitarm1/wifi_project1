7aef492a7dc84ec80ac474e29eb3000267a9f224
dmarinov <dimitar.m.marrinov@abv.com>
2018-10-21 21:24:10 +0300

    Svarzvane kam 1 WIFI i pokazvane na indikacia na IP adresa
    Signed-off-by: dmarinov <dimitar.m.marrinov@abv.com>

--- CallSDKFunctions/CallSDKFunctions.ino
+++ CallSDKFunctions/CallSDKFunctions.ino
@@ -9,6 +9,7 @@
 #include <SSD1306Brzo.h>
 #include <SSD1306Spi.h>
 #include <SSD1306Wire.h>
+#include <EEPROM.h>
 
 
 /*
@@ -44,8 +45,16 @@ const char *ssid = "ESPap";
 const char *password = "thereisnospoon";
 bool IsConfigured = false;
 
+char ssid_name[20];
+char ssid_password[20];
+bool startHotSpot = true;
+char counter;
+
 ESP8266WebServer server(80);
 
+void handleNotFound(){
+  server.send(404, "text/plain", "404: Not found"); // Send HTTP status 404 (Not Found) when there's no handler for the URI in the request
+}
 
 void handleBody() {
    if (server.hasArg("ssid")== true){ //Check if body received
@@ -90,26 +99,99 @@ void setup() {
   //Serial.println();
   //Serial.print("Configuring access point...");
   /* You can remove the password parameter if you want the AP to be open. */
-  WiFi.softAP(ssid/*, password*/);
-
-  IPAddress myIP = WiFi.softAPIP();
-  //Serial.print("AP IP address: ");
-  //Serial.println(myIP);
-  server.on("/", HTTP_GET, handleRoot);
-  server.on("/action_page.php",HTTP_POST, handleBody); //Associate the handler function to the path
-  server.begin();
-  //Serial.println("HTTP server started");
+  EEPROM.begin(512); 
+  for(unsigned char i  = 1; i < 20; i++)
+  {
+    ssid_name[i-1] = EEPROM.read(i);
+  }
+  if(strlen(ssid_name) > 0)
+  {
+    for(unsigned char i  = 21; i < 40; i++)
+    {
+      ssid_password[i-21] = EEPROM.read(i);
+      if( ssid_password[i-21] == ' ')
+      {
+        ssid_password[i-21] = 0; // Space means no password!!!
+        break;
+      }
+    } 
+    startHotSpot = true;
+  }
+  if(digitalRead(9))
+  {
+    startHotSpot = true;
+  }
+  
   display.init();
   display.flipScreenVertically();
   display.setContrast(255);
   display.clear();
   display.setFont(ArialMT_Plain_10);
   display.setTextAlignment(TEXT_ALIGN_CENTER_BOTH);
-  display.drawString(display.getWidth()/2, display.getHeight()/2 - 10, "Solar Power");
-  display.display();
+  display.drawString(display.getWidth()/2, 10, "Solar Power");
+  startHotSpot = false;
+  if(startHotSpot)
+  {
+    sprintf(ssid_name, "Solarium_%08x",ESP.getChipId());
+    WiFi.softAP(ssid_name/*, password*/);
+    IPAddress myIP = WiFi.softAPIP();
+    //Serial.print("AP IP address: ");
+    //Serial.println(myIP);
+   
+    display.drawString(display.getWidth()/2, 25, "HotSpot:");
+    display.drawString(display.getWidth()/2, 35, ssid_name);
+    display.display();
+  }
+  else
+  {
+    WiFi.mode(WIFI_STA);
+    WiFi.setAutoReconnect(true);    
+    //WiFi.begin(ssid_name, ssid_password);
+    WiFi.begin("LENX");
+    display.setTextAlignment(TEXT_ALIGN_LEFT);
+    display.drawString(1, 25, "SSID:");
+    display.drawString(35, 25, "ssid_name");   
+  }
+  server.on("/", HTTP_GET, handleRoot);
+  server.on("/action_page.php",HTTP_POST, handleBody); //Associate the handler function to the path  
+  server.onNotFound(handleNotFound);           // When a client requests an unknown URI (i.e. something other than "/"), call function "handleNotFound"
+  server.begin();
+  //Serial.println("HTTP server started");
+ 
+  //display.drawString(display.getWidth()/2, display.getHeight()/2 - 10, "Solar Power");
+ 
 }
 
 void loop() {
+  if((!startHotSpot) && (WiFi.status() != WL_CONNECTED))
+  {
+    delay(500);          
+    display.setColor(BLACK);
+    display.fillRect(0, 35, display.getWidth(), 20);
+    display.setColor(WHITE);
+    counter++;     
+    switch(counter)
+    {        
+      case 1:
+        display.drawString(2, 45, "Connecting.");
+        break;
+      case 2:
+        display.drawString(2, 45, "Connecting...");
+        break;        
+      case 3:             
+      default:
+        display.drawString(2, 45, "Connecting.....");          
+        counter = 0;
+        break;
+    }
+      
+    display.display();
+    
+    return;  
+  }  
+  display.clear();
+  display.drawString(2, 45, WiFi.localIP().toString());
+  display.display();
   server.handleClient();
   
   // Call Espressif SDK functionality - wrapped in ifdef so that it still
@@ -123,3 +205,4 @@ void loop() {
    // display.display();
   //delay(1000);
 }
+
